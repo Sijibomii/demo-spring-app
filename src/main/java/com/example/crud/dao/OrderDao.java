f60e3d1b7e05ae355145dfb00f19d9d8c29d7524
Ajayi Sijibomi <sijibomiolajubu@gmail.com>
2023-04-23 09:22:22 +0100

    order dao

---
+++ src/main/java/com/example/crud/dao/OrderDao.java
@@ -0,0 +1,10 @@
+package com.example.crud.dao;
+
+import com.example.crud.entity.Order;
+import com.example.crud.dao.base.BaseDao;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderDao extends BaseDao<Order> {
+    
+}
