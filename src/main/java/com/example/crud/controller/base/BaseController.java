6655792786ef59fb39798852d3b258ef5b78adb4
Ajayi Sijibomi <sijibomiolajubu@gmail.com>
2023-04-25 08:10:34 +0100

    base controller start up

--- src/main/java/com/example/crud/controller/base/BaseController.java
+++ src/main/java/com/example/crud/controller/base/BaseController.java
@@ -9,5 +9,38 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
 public class BaseController {
-    
+    private static Log log = LogFactory.getLog(BaseController.class);
+
+    protected MessageResult success() { 
+        return new MessageResult(0, "SUCCESS");
+    }
+
+    protected MessageResult success(String msg) {
+        return new MessageResult(0, msg);
+    }
+
+    protected MessageResult success(String msg, Object obj) {
+        MessageResult mr = new MessageResult(0, msg);
+        mr.setData(obj);
+        return mr;
+    }
+
+    protected MessageResult success(Object obj) {
+        MessageResult mr = new MessageResult(0, "SUCCESS");
+        mr.setData(obj);
+        return mr;
+    }
+
+    protected MessageResult error(String msg) {
+        return new MessageResult(500, msg);
+    }
+
+    protected MessageResult error(int code, String msg) {
+        return new MessageResult(code, msg);
+    }
+
+    protected String request(HttpServletRequest request, String name) {
+        // Removes control characters (char <= 32) from both ends of this String returning an empty String ("") if the String is empty ("") after the trim or if it is null.
+        return StringUtils.trimToEmpty(request.getParameter(name));
+    }
 }
