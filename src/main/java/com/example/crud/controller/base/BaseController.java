d2d17dd5a2345a9088e4f0c5d37896725226f5ea
Ajayi Sijibomi <sijibomiolajubu@gmail.com>
2023-04-25 08:08:42 +0100

    fixed org.apache.commons.lang3.StringUtils

---
+++ src/main/java/com/example/crud/controller/base/BaseController.java
@@ -0,0 +1,13 @@
+package com.example.crud.controller.base;
+
+import com.example.crud.util.MessageResult;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
+public class BaseController {
+    
+}
